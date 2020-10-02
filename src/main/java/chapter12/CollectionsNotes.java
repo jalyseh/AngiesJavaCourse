f03df0fd6252b9014d4c85f1984dfecb1afd0fee
jalyseh <hammettjadyn@gmail.com>
2020-10-01 23:12:14 -0400

    Adding chapter 12

---
+++ src/main/java/chapter12/CollectionsNotes.java
@@ -0,0 +1,38 @@
+package chapter12;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CollectionsNotes {
+        //SET
+    //unordered collection of unique objects
+    //Interface
+
+        //LIST
+    //Ordered elements which can be accessed by position
+
+        //QUEUE
+    //Ordered elements for processing. Access in the order elements were added.
+    //(FIFO: first in - first out)
+    //First element = head, Last element = tail
+    //.remove(); will remove first in the que
+
+        //MAP
+    //does not inherit from Collection interface
+    //.put instead of .add
+    /*if you call put passing in a key that already exists, the map will
+    update with latest value*/
+    //.entrySet gives entire collection
+    //unordered unique key-value pairs
+
+
+
+
+
+
+
+
+
+
+}
